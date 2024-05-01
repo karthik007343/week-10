576e9cca6e675faf999f5c13b098a612434fbb8f
karthik007343 <159223934+karthik007343@users.noreply.github.com>
2024-05-01 15:02:54 +0530

    Update mapinter.java

--- mapinter.java
+++ mapinter.java
@@ -5,8 +5,8 @@ public static void main(String[] args) {
 Map<String, String> SId = new HashMap<>();
 SId.put("karthik, "501");
 SId.put("siddu", "523");
-SId.put("harshi", "566");
-SId.put("nithish", "550");
+SId.put("harsha", "566");
+SId.put("kiran", "550");
 for (Map.Entry<String, String> entry : SId.entrySet()) {
 String name = entry.getKey();
 String Id = entry.getValue();
@@ -15,7 +15,7 @@ System.out.println(name + " ID: " + Id);
 System.out.println("\n");
 System.out.println("Changing -> karthik : 501 - l501");
 SId.put("karthikl501");
-System.out.println("Removing -> nithish : 550");
+System.out.println("Removing -> kiran : 550");
 SId.remove("nithish");
 boolean sidd = SId.containsKey("siddu");
 System.out.println("Is there any Student Id with siddu ? --> " + sidd);
